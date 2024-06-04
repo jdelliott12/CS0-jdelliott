a61ac4f1acd601e944fea9fb6a7332c50bc0aa93
Jacob Elliott <jdelliott.mavs@coloradomesa.com>
2024-06-04 12:36:52 -0600

    HW 2.14

--- firsthw.py
+++
@@ -1,3 +0,0 @@
-# Jake Elliott first Python out
-print("Hello World, my program prints two numbers:")
-print(24+18)
---
+++ hw2.14.py
@@ -0,0 +1,14 @@
+# Jake Elliott assignment problem 1 from section 2.14
+
+n1 = "All"
+n2 = "work"
+n3 = "and"
+n4 = "no"
+n5 = "play"
+n6 = "makes"
+n7 = "Jack"
+n8 = "a"
+n9 = "dull"
+n10 = "boy"
+
+print(n1, n2, n3, n4, n5, n6, n7, n8, n9, n10)
