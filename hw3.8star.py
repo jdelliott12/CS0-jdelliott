10d958d5d81383fa610accbd20175bcf1521478b
Jacob Elliott <jdelliott.mavs@coloradomesa.com>
2024-06-04 14:49:08 -0600

    HW 3.8 Star

---
+++ hw3.8star.py
@@ -0,0 +1,28 @@
+import turtle
+
+screen = turtle.Screen()
+
+screen.bgcolor("white")
+
+star = turtle.Turtle()
+
+star.color("black")
+
+star.hideturtle()
+
+star.forward(100)
+star.right(144)
+
+star.forward(100)
+star.right(144)
+
+star.forward(100)
+star.right(144)
+
+star.forward(100)
+star.right(144)
+
+star.forward(100)
+star.right(144)
+
+turtle.done()
